e0dd1dfed1a59ba938fb2dc4173e7251459581af
Shakthivel <shakthi@scripteco.com>
2025-08-07 19:48:33 +0530

    commented out

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 
 //Basic Express.js server setup for handling PUT requests
-import express from "express";
-const app = express()
-const port = 3000
+// import express from "express";
+// const app = express()
+// const port = 3000
 
-app.use(express.json())
+// app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Welcome to the PUT example!")
-})
+// app.get("/", (req, res) => {
+//     res.send("Welcome to the PUT example!")
+// })
 
-app.put("/users/:id", (req, res) => {
-    const userId = req.params.id
-    res.send(`User ${userId} updated with data: ${JSON.stringify(req.body)}`)
-})
+// app.put("/users/:id", (req, res) => {
+//     const userId = req.params.id
+//     res.send(`User ${userId} updated with data: ${JSON.stringify(req.body)}`)
+// })
 
 
-app.listen(port, ()=> {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+// app.listen(port, ()=> {
+//     console.log(`Server is running on http://localhost:${port}`)
+// })
 
 // challenge-1 with put requests
 
-import express from "express";
+// import express from "express";
 
-const app = express()
-const port = 3000;
+// const app = express()
+// const port = 3000;
 
-app.use(express.json());
+// app.use(express.json());
 
-app.put("/profile/:username", (req, res) => {
-    const username = req.params.username;
-    const userData = req.body
-    res.send(`Profile of user ${username} updated with : ${JSON.stringify(userData)}`)
-})
+// app.put("/profile/:username", (req, res) => {
+//     const username = req.params.username;
+//     const userData = req.body
+//     res.send(`Profile of user ${username} updated with : ${JSON.stringify(userData)}`)
+// })
 
 
-app.listen(port, ()=> {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+// app.listen(port, ()=> {
+//     console.log(`Server is running on http://localhost:${port}`);
+// })
 
 // challenge-2 with put requests
 
